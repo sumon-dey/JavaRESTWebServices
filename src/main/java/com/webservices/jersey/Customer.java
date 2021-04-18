c51180b348eeddc54a9d5678083026d0510190df
Sumon <sdtechie22@gmail.com>
2021-04-18 11:22:58 +0530

    Added Person service intereface and implementation

--- src/main/java/com/webservices/jersey/Customer.java
+++ src/main/java/com/webservices/jersey/Customer.java
@@ -1,6 +1,10 @@
 package com.webservices.jersey;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,6 +21,33 @@ public class Customer {
 		System.out.println("Customer name is: " + customerName);
 		System.out.println("Customer city is: " + customerCity);
 		return "<h1>Hello WebServices</h1>";
+	}
+
+	@PUT
+	@Path("{name}/{city}")
+	public void putCustomerInformation(@PathParam("name") String customerName, @PathParam("city") String customerCity) {
+
+	}
+
+	@POST
+	@Path("{name}/{city}")
+	public void postCustomerInformation(@PathParam("name") String customerName,
+			@PathParam("city") String customerCity) {
+
+	}
+
+	@DELETE
+	@Path("{name}/{city}")
+	public void deleteCustomerInformation(@PathParam("name") String customerName,
+			@PathParam("city") String customerCity) {
+
+	}
+
+	@HEAD
+	@Path("{name}/{city}")
+	public void headCustomerInformation(@PathParam("name") String customerName,
+			@PathParam("city") String customerCity) {
+
 	}
 
 }
